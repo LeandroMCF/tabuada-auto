0162d46aa002a250d9cfa7fea5c3059d4b81a633
Leandro Cavalcanti <leand@DESKTOP-LFIP8ID.(none)>
2020-11-23 15:35:35 -0300

    Tabu auto

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tabuada_auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada 1-10");
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine($"Tabuada do {i}");
+                for (var cont = 0; cont < 11; cont++)
+                {
+                    int resu = i * cont;
+                    Console.WriteLine($"{i} x {cont} = {resu}");
+                }
+                Console.WriteLine("----------------");
+            }
+        }
+    }
+}
